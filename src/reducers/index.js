4833a420f24a95c609457772eff7d3eb2339813a
RobertS <rshugart@protonmail.com>
2019-02-28 14:09:27 -0500

    Created songsReducers and SelectedSongReducer components

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+  return [
+    { title: "Loser", duration: "4:05" },
+    { title: "Ironic", duration: "2:15" },
+    { title: "Voice of Truth", duration: "3:45" },
+    { title: "Cowboy", duration: "5:05" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
